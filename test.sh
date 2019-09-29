bb03340b46506a2d75c61a38e7654129667ca68a
htlsne <htlsne23@gmail.com>
2019-09-29 16:43:24 +0900

    Update script

--- test.sh
+++ test.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+if [ "$1" = "-v" ]; then
+  STDERR_FLAG=1
+fi
+
 for i in {01..99}; do
   if [ ! -f test/i${i}.txt ]; then
     exit
   fi
 
-  ACTUAL=`cargo run < test/i${i}.txt`
+  if [ -z "$STDERR_FLAG" ]; then
+    ACTUAL=`cargo run < test/i${i}.txt 2>/dev/null`
+  else
+    ACTUAL=`cargo run < test/i${i}.txt`
+  fi
   EXPECTED=`cat test/o${i}.txt`
+
+  echo "expected: $EXPECTED"
+  echo "actual:   $ACTUAL"
 
   if [ "$ACTUAL" = "$EXPECTED" ]; then
     echo OK
