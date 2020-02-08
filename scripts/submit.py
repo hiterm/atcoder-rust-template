624b56de095ef1d81eb867afbe9d38aa8ce9b63a
htlsne <htlsne23@gmail.com>
2020-02-08 23:48:09 +0900

    Add submit.py

---
+++ scripts/submit.py
@@ -0,0 +1,30 @@
+#!/bin/python
+
+import subprocess
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('problem')
+args = parser.parse_args()
+
+problem = args.problem
+
+# detect debug prints
+file = 'src/bin/{}.rs'.format(problem)
+p1 = subprocess.Popen(['grep', '-e', 'eprintln!', '-e', 'dbg!', file],
+                      stdout=subprocess.PIPE)
+p2 = subprocess.Popen(['grep', '-v', '^ *//'],
+                      stdin=p1.stdout, stdout=subprocess.PIPE)
+p1.stdout.close()
+output = p2.communicate()[0].decode()
+if output != "":
+    print()
+    print("!!!!Please remove debug prints!!!!")
+    print(output)
+    sys.exit()
+
+subprocess.run(
+    ['rustup', 'run', 'stable', 'cargo', 'atcoder', 'submit', '--bin', problem]
+)
