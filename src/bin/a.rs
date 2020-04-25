07b9705ea1182cac2f0d8ad06d967de1391e642a
htlsne <htlsne23@gmail.com>
2020-04-25 18:20:02 +0900

    Update template

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,69 +1,28 @@
-#[allow(unused_imports)]
-use std::io::Write;
-
-// {{{1
-#[allow(unused)]
-macro_rules! debug {
-    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
-}
-#[allow(unused)]
-macro_rules! debugln {
-    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
-}
-
-
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let mut s = {
-            use std::io::Read;
+//{{{
+#[allow(unused_macros)]
+macro_rules! getl {
+    ( $( $t:ty ),* ) => {
+        {
             let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
+            std::io::stdin().read_line(&mut s).unwrap();
+            let s = s.trim_right();
+            let mut ws = s.split_whitespace();
+            ($(ws.next().unwrap().parse::<$t>().unwrap()),*)
+        }
     };
 }
 
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
+#[allow(unused_macros)]
+macro_rules! getl_vec {
+    ( $t:ty ) => {{
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        let s = s.trim_right();
+        s.split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect::<Vec<$t>>()
+    }};
 }
+//}}}
 
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
-// }}}
-
-fn main() {
-    input! {
-    }
-}
+fn main() {}
